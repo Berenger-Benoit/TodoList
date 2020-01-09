5383a7369f1d2b31db3b77dff50adc950377f419
Berenger-Benoit <berenger.benoit@gmail.com>
2020-01-09 22:31:47 +0100

    change Entity User

--- TodoList/src/Form/RegistrationFormType.php
+++ TodoList/src/Form/RegistrationFormType.php
@@ -9,16 +9,21 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('username', TextType::class, [
+                'required' => true,
+                'label' => 'Nom'
+            ])
             ->add('email', EmailType::class, [
                 'required' => true,
                 'label' => 'Email'
